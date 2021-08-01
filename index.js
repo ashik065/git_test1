002acc688fb03901c8a5ff6e2b949141a4779909
ashik065 <abdullahashik065@gmail.com>
2021-08-01 20:20:43 +0600

    Added js file

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+
+var number1 = 10;
+
+var number2 = true;
+var number3 = "50";
+
+total = number1 + number3;
+
+console.log(total);
+
+console.log(typeof(number2));
+
+
